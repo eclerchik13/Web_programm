271deeb9ab35681ca18b6e94c1ab46232a47e5f1
eclerchik13 <ler.poter2014@yandex.ru>
2020-09-20 20:43:34 +0300

    1

--- Task_1.js
+++ Task_1.js
@@ -1,13 +1,11 @@
 
-function CamelRegister(par1){
+function CamelRegister(par1)
+{
     let NewArr = par1.split(" ");
-    console.log(NewArr)
     let NewString = "";
     for (let i = 0; i < NewArr.length ; i++ )
     {
         NewString = NewString + ((NewArr[i])[0].toUpperCase()) + NewArr[i].slice(1,NewArr[i].length + 1)
     }
-    console.log(NewString)
+    return(NewString)
 }
-
-CamelRegister("Hello world my little girl");
