78d64b51c5c1aea90d0fd885af66314fde1f2cee
eclerchik13 <ler.poter2014@yandex.ru>
2020-10-31 12:45:50 +0300

    add new get and post request, session cookie

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const fs = require("fs");
-const hbs = require("hbs")
+const hbs = require("hbs");
+const bodyParser = require('body-parser')
+const keys = require('./config/keys')
+
+const app = express()
+const cookieSession = require("cookie-session");
+const cookieParser = require('cookie-parser');// module for parsing cookies
+const flash = require('connect-flash');
+
+const ConnectBase = require("./users").ConnectBase;
+const Uri = require("./users").Uri
+
 
 const function_1 = require("./Task_1")
 const function_2 = require("./Task_2")
@@ -8,14 +19,65 @@ const function_3 = require("./Task_3")
 
 const title = "Web Program"
 const port_ = 3000
-const app = express()
+const passport = require('./config/passportJS').passport
+require("passport-local")
+
+ConnectBase(Uri);
 
 app.set("view engine", "hbs");
 hbs.registerPartials("./views/partials/")
-
 app.use(express.static("public"));
 
-app.use(LoggingToConsole); // middleware Logging Request
+app.use(flash());           //управление сообщения на flash
+                            // эти сообщения сохраняются в сессии,нужно для ошибок в регистраци и логине
+                            //Сессия - промежуток времени, в теч которого пользователь находится на сайте
+app.use(cookieSession({
+    name: "session",        //по умолчанию
+    keys: [keys.session.cookieKey],    //Список ключей, используемых для подписи и проверки значений файлов cookie
+    maxAge: 900000,
+    secure: false,//логическое значение, указывающее, должен ли cookie пересылаться только по HTTPS
+    signed: true//логическое значение, указывающее, должен ли быть подписан файл cookie
+}));
+app.use(cookieParser());
+
+app.use(bodyParser.json())  //для обработки post запросов
+                            // извлекает всю часть тела вход потока запросов
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(passport.initialize()); //аутентификация для nodejs
+app.use(passport.session());    //If your application uses persistent login sessions,
+                                // passport.session() middleware must also be used.
+
+app.use(LoggingToConsole);      // middleware Logging Request
+
+
+app.get('/registration',function (req,res){
+    res.render('registration.hbs',{
+        path: "/registration",
+        errorMessage: req.flash('error')
+    })
+    //console.log(req.flash());
+})
+
+app.get('/signIn',function(req,res){
+        res.render('signIn.hbs',{
+            path: '/signIn',
+            errorMessage: req.flash('error')
+        })
+})
+
+app.post('/signIn', passport.authenticate('authentication',{
+    successRedirect: '/',
+    failureRedirect: '/signIn',
+    failureFlash: true
+}))
+
+
+app.post('/registration',passport.authenticate('registration',{
+    successRedirect:'/signIn',
+    failureRedirect: '/registration',
+    failureFlash: true
+}))
 
 app.get('/',function(request,response){
     response.render("home.hbs", {
@@ -34,7 +96,10 @@ app.get('/api/LukinaValeria/lab1/1_Input',function (request,response){
     })
 })
 
-app.get("/api/LukinaValeria/lab1/1_Result",function (req,res){
+app.get("/api/LukinaValeria/lab1/1_Result",passport.authenticate('cookie-session',{
+    failureRedirect: '/signIn',
+    failureFlash: true
+}), function (req,res){
     let answer = function_1.CamelRegister(req.query.String);
     res.render('camelRegisterResult.hbs',{
         title: title,
@@ -50,7 +115,11 @@ app.get('/api/LukinaValeria/lab1/2_Input', function (request,response) {
     })
 })
 
-app.get("/api/LukinaValeria/lab1/2_Result", AuthUser,function(request,response){
+//app.get("/api/LukinaValeria/lab1/2_Result", AuthUser,function(request,response){
+app.get("/api/LukinaValeria/lab1/2_Result", passport.authenticate('cookie-session',{
+    failureRedirect: '/signIn',
+    failureFlash: true
+}), function(request,response){
     let answer = function_2.SortString(request.query.StringForSort)
     response.render('sortFunctionResult.hbs', {
         title: title,
@@ -66,7 +135,11 @@ app.get('/api/LukinaValeria/lab1/3_Input',function (request,response){
     })
 })
 
-app.get('/api/LukinaValeria/lab1/3_Result', AuthUser, function(request,response,next) {
+//app.get('/api/LukinaValeria/lab1/3_Result', AuthUser, function(request,response,next) {
+app.get('/api/LukinaValeria/lab1/3_Result',passport.authenticate('cookie-session',{
+    failureRedirect: '/signIn',
+    failureFlash: true
+}),function(request,response,next) {
     if (Number(request.query.month_)> 12  ||
         Number(request.query.month_) < 0 ||
         Number(request.query.date_) < 1 ||
@@ -75,7 +148,6 @@ app.get('/api/LukinaValeria/lab1/3_Result', AuthUser, function(request,response,
         Number(request.query.month_) == null ||
         Number(request.query.date_) == null)
     {
-        //throw new Error('403_WrongValue')
         next(new Error('403_WrongValue'))
         return
     }
@@ -96,7 +168,11 @@ app.use((req, res, next) => {
 })
 
 app.use(LogError);
-app.listen(port_);
+let ser = app.listen(port_,()=>{
+    console.log("Server started at", port_)
+});
+
+//ser.close()
 
 ///////////////// - middleware - /////////////////
 
@@ -136,7 +212,6 @@ function LoggingToConsole(req,res,next) {
 function LogError(err,req,res,next) {
     switch (err.message){
         case '403_WrongValue':
-                //res.status(403).send("<h1>Error: 403 </h1> Incorrect data entered! ");
             res.render("errors.hbs", {
                 title: title,
                 tit: "ERROR:",
@@ -145,7 +220,6 @@ function LogError(err,req,res,next) {
             });
             break;
         case '403_Access':
-                //res.status(403).send("<h1>Error: 403 </h1> You don't have acces");
             res.render("errors.hbs", {
                 title:title,
                 tit: "ERROR:",
@@ -154,7 +228,6 @@ function LogError(err,req,res,next) {
             });
             break;
         case '404':
-                //res.status(404).send("<h1>Error: 404 </h1> This page does not exist");
             res.render("errors.hbs", {
                 title: title,
                 tit: "ERROR:",
@@ -163,7 +236,6 @@ function LogError(err,req,res,next) {
             });
             break;
         default:
-                //res.status(500).send("<h1>Error: 505 </h1> Internal Server Error")
             res.render("errors.hbs", {
                 title: title,
                 tit: "ERROR:",
